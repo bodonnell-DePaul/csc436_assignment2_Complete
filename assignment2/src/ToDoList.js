008c885813ad23608c8c67d01afa645f3b7d1c16
Brian O'Donnell <brian.m.odonnell@gmail.com>
2024-09-12 18:22:00 +0000

    wrote tests for assignment

--- assignment2/src/ToDoList.js
+++ assignment2/src/ToDoList.js
@@ -10,22 +10,22 @@ function TodoList() {
     aliquip ex ea commodo consequat. Duis aute irure dolor in
     reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
     pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-    culpa qui officia deserunt mollit anim id est laborum.`, dueDate: '2024-04-03' },
+    culpa qui officia deserunt mollit anim id est laborum.`, dueDate: '2024-10-03' },
 
     { title: 'Todo 2', description: `Sed auctor nunc nec nisi ultrices, in molestie nibh mattis. 
     Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
     Suspendisse potenti. Nullam nec nibh nibh. Nullam quis nisl nec nunc congue mollis. 
-    Vivamus nec nisi nec nunc mattis molestie. Sed auctor nunc nec nisi ultrices, in molestie nibh mattis.`, dueDate: '2024-04-06' },
+    Vivamus nec nisi nec nunc mattis molestie. Sed auctor nunc nec nisi ultrices, in molestie nibh mattis.`, dueDate: '2024-09-17' },
 
     { title: 'Todo 3', description: `Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
     Suspendisse potenti. Nullam nec nibh nibh. Nullam quis nisl nec nunc congue mollis. Vivamus nec nisi nec nunc mattis molestie. 
     Sed auctor nunc nec nisi ultrices, in molestie nibh mattis. Pellentesque habitant morbi tristique 
-    senectus et netus et malesuada fames ac turpis egestas.`, dueDate: '2024-04-09' },
+    senectus et netus et malesuada fames ac turpis egestas.`, dueDate: '2024-09-15' },
 
     { title: 'Todo 4', description: `Suspendisse potenti. Nullam nec nibh nibh. Nullam quis nisl nec nunc congue mollis. 
     Vivamus nec nisi nec nunc mattis molestie. Sed auctor nunc nec nisi ultrices, in molestie nibh mattis. 
     Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Suspendisse potenti. 
-    Nullam nec nibh nibh. Nullam quis nisl nec nunc congue mollis. `, dueDate: '2024-04-11' },
+    Nullam nec nibh nibh. Nullam quis nisl nec nunc congue mollis. `, dueDate: '2024-09-13' },
   ];
 
   const getVariant = (dueDate) => {
@@ -48,7 +48,7 @@ function TodoList() {
     <Container className="p-3">
       <Row className="mb-5">
         <Col>
-          <h1 className="text-center">Assignment 2: Brian's ToDo List</h1>
+          <h1 className="text-center">Assignment 2: ToDo List</h1>
         </Col>
       </Row>
       <Row className="d-flex align-items-start">
